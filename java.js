fa3b2c8b07ac49e240ebe0ae1597b8dcb9087274
biggyinvestments <bot.fexxy11@gmail.com>
2024-09-28 11:52:33 -0700

    Add files via upload

--- java.js
+++ java.js
@@ -324,3 +324,23 @@ document.addEventListener('DOMContentLoaded', function() {
             });
 });
 
+
+
+
+
+
+const menuBtn = document.querySelector('.menu-btn');
+        const topMenu = document.getElementById('topMenu');
+        const overlay = document.getElementById('overlay');
+
+        // Toggle Menu and Overlay
+        menuBtn.addEventListener('click', () => {
+            topMenu.classList.toggle('active');
+            overlay.classList.toggle('active');
+        });
+
+        // Close Menu when clicking outside
+        overlay.addEventListener('click', () => {
+            topMenu.classList.remove('active');
+            overlay.classList.remove('active');
+});
