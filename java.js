bea1dbc40ac08e9c5560fb1b200386bd96ef65e1
biggyinvestments <bot.fexxy11@gmail.com>
2024-10-21 08:29:45 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -201,7 +201,15 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.log('Service Worker registration failed:', error);
+    });
+}
 
 
 
---
+++ service-worker.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = 'Biggyassets-cache-v4';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/about-us.html',
+  '/signup.html',
+  '/signin.html',
+  '/bonus.html',
+  '/confirm.html',
+  '/dashboard.html',
+  '/earning-history.html',
+  '/confirm.html',
+  '/invest.html',
+  '/java.js',
+  '/expiring.html',
+  '/faq.html',
+  '/fund-user.html',
+  '/get-account-details.html',
+  '/user-withdrawal.html',
+  '/profile.html',
+  '/users.html',
+  '/pending-deposit.html',
+  '/pending-withdrawals.html',
+  '/withdrawal-history.html',
+  '/withdrawal-request.html',
+  '/css/about-us.css',
+  '/css/forms.css',
+  '/css/admindash.css',
+  '/css/dashboard.css',
+  '/css/faq.css',
+  '/css/index.css',
+  '/css/invest.css',
+  '/css/signup.css',
+  '/images/biggy logo medium.png',
+  '/images/biggy logo large.png',
+  '/images/biggy logo small.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
